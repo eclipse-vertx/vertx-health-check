0ed5740404a862e8e2dc33a392730773664d75ce
Julien Viet <julien@julienviet.com>
2025-06-19 07:51:13 +0200

    Regen code

---
+++ src/main/generated/io/vertx/ext/healthchecks/StatusConverter.java
@@ -0,0 +1,53 @@
+package io.vertx.ext.healthchecks;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.impl.JsonUtil;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
+/**
+ * Converter and mapper for {@link io.vertx.ext.healthchecks.Status}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.ext.healthchecks.Status} original class using Vert.x codegen.
+ */
+public class StatusConverter {
+
+
+  private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
+  private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
+
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, Status obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "data":
+          if (member.getValue() instanceof JsonObject) {
+            obj.setData(((JsonObject)member.getValue()).copy());
+          }
+          break;
+        case "ok":
+          if (member.getValue() instanceof Boolean) {
+            obj.setOk((Boolean)member.getValue());
+          }
+          break;
+        case "procedureInError":
+          if (member.getValue() instanceof Boolean) {
+            obj.setProcedureInError((Boolean)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+   static void toJson(Status obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+   static void toJson(Status obj, java.util.Map<String, Object> json) {
+    if (obj.getData() != null) {
+      json.put("data", obj.getData());
+    }
+    json.put("ok", obj.isOk());
+    json.put("procedureInError", obj.isProcedureInError());
+  }
+}
